6f90af3a2565467e8b030d9f05105756926676a5
GrigoriyShitov <grisha.shitob1@gmail.com>
2024-04-21 14:41:55 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ContactsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()//Contacts
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(Contact contact)//Contacts
+        {
+            if (ModelState.IsValid)
+            {
+                return Redirect("/");
+            }
+            return View("Index");
+        }
+    }
+}
